72a4d6f90cb696059ea5f783cdc0d3c8239b37aa
MrNihalT <nihal.chiyoor@gmail.com>
2026-05-20 18:06:53 +0530

    updated the explroe page

--- toddy_shop_frontend/next.config.ts
+++ toddy_shop_frontend/next.config.ts
@@ -1,19 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        pathname: "/aida-public/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+                pathname: "/aida-public/**",
+            },
+            {
+                protocol: "https",
+                hostname: "images.unsplash.com",
+            },
+            {
+                protocol: "https",
+                hostname: "flyriatravel.com",
+                pathname: "/wp-content/**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
